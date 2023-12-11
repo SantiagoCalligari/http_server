3a0c0d4512aa5177072a6a57b937ca46596b6a19
Santiago Calligari <89709491+SantiagoCalligari@users.noreply.github.com>
2023-12-10 22:24:00 -0300

    anda el tokio

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
 use std::str;
+use std::io;
 
-fn handle_connection(mut stream: &TcpStream)  {
-    let mut streamOpen = true;
-    let mut buffer = [0; 512];
-    stream.write(b"HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello World!");
-    println!("You are connected");
-    while stream.read(&mut buffer).unwrap() != 0 {
-        let buffer = str::from_utf8(&buffer).unwrap();
-        println!("{}",buffer);
-    }
-}
-
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.0:8088")?;
-    for stream in listener.incoming() {
-      match stream {
-          Ok(stream) => {
-              handle_connection(&stream);
-          }
-          Err(e) => {
-              eprintln!("An error has ocurred: {}", e);
-          }
-      }
+async fn handle_connection(mut stream: TcpStream) -> std::io::Result<()>{
+    let mut buffer = [0; 1024];
+    stream.writable().await?;
+    stream.try_write(b"HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello World!")?;
+    loop {
+        stream.readable().await?;
+        match stream.try_read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => {
+                println!("{:?}",n);
+                println!("{}", str::from_utf8(&buffer).unwrap());
+            }
+            Err(ref e ) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
     }
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("192.168.100.224:6969").await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        handle_connection(socket).await.unwrap_or_else( |e| {
+            println!("Error: {}", e);
+        });
+    }
 }
